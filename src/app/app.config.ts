2a01a124503052ece125840309863a1b459a128f
Sanjai <sannila1527@gmail.com>
2026-01-17 08:23:15 +0530

    fav icon added.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { withInMemoryScrolling } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes, withInMemoryScrolling({
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled'
+    }))
   ]
 };
